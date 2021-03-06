a7cb078725141bcb0077ad60b0448d2243b2353e
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2021-03-07 00:01:18 +0100

    hmmmm

---
+++ js/ui/dialogs.js
@@ -0,0 +1,49 @@
+function showBoardBoardDialog(event, id = null) {
+    if (event.srcElement == null)
+        event.srcElement = event.target;
+    if (drags.dragItem != null && (event.srcElement == drags.dragItem[0] || event.srcElement.parentNode == drags.dragItem[0]))
+        return;
+    if (id == null)
+        id = dataId(event.srcElement.parentNode);
+    set_board(id);
+}
+function listTitleClicked(event) {
+    if (event.srcElement == null)
+        event.srcElement = event.target;
+    let titleText = event.srcElement;
+    $(titleText).focus();
+    try {
+        document.execCommand('selectAll', false, null);
+    }
+    catch (e) { }
+    log('Title click');
+}
+function listTitleBlur(event) {
+    if (event.srcElement == null)
+        event.srcElement = event.target;
+    let titleText = event.srcElement;
+    log('Title blur');
+}
+function newReferenceBtn(event) {
+    let refer = window.prompt("Write/Paste id of board to reference:");
+    if (refer == null)
+        return;
+    if (pb.boards[refer] == null) {
+        alert("ID doesn't exist :(");
+        return;
+    }
+    if (pb.boards[refer].type == BoardType.List) {
+        alert("Cant embed lists into boards.");
+        return;
+    }
+    if (event.srcElement == null)
+        event.srcElement = event.target;
+    let lst = event.srcElement.parentNode.parentNode.parentNode;
+    let lstId = dataId(lst);
+    pb.boards[lstId].content.push(refer);
+    clearBoards(lst);
+    loadList(lst, lstId);
+    pb.boards[refer].attributes['references']++;
+    hideOptionsDialog();
+    sync.saveAll();
+}
