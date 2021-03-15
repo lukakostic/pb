f7548adde4982587d5dfc03f7ad261c6cebcdb84
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2021-03-15 02:46:18 +0100

    changed visuals

--- js/_manager.js
+++ js/_manager.js
@@ -2,7 +2,7 @@ let siteUrl = "https://lukakostic.github.io/pb/";
 let pb = null;
 let board = "";
 let currentVersion = 3.1;
-console.log(currentVersion, " : ", 'ff');
+console.log(currentVersion, " : ", 'changed list visuals');
 window.onhashchange = function () {
     set_board(boardFromUrl(url()));
 };
