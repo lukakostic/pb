a343efbed53894072559573ae3731155536ddd1e
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2021-03-15 02:32:03 +0100

    tile sizes

--- js/_manager.js
+++ js/_manager.js
@@ -2,7 +2,7 @@ let siteUrl = "https://lukakostic.github.io/pb/";
 let pb = null;
 let board = "";
 let currentVersion = 3.1;
-console.log(currentVersion, " : ", 'rrr');
+console.log(currentVersion, " : ", 'ff');
 window.onhashchange = function () {
     set_board(boardFromUrl(url()));
 };
