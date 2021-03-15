b726a7ab10b2c4aed7147a77de6ded0de278d127
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2021-03-15 05:44:01 +0100

    AAAAAAAAAAA

--- js/_manager.js
+++ js/_manager.js
@@ -2,7 +2,7 @@ let siteUrl = "https://lukakostic.github.io/pb/";
 let pb = null;
 let board = "";
 let currentVersion = 3.1;
-console.log(currentVersion, " : ", 'HHHHHHHH!');
+console.log(currentVersion, " : ", 'Ok visuals are nice ig');
 window.onhashchange = function () {
     set_board(boardFromUrl(url()));
 };
